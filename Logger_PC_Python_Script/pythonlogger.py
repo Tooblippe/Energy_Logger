ae040250be97e836c1a1772d3cd5bc5850c8f177
Tobie Nortje <tobie.nortje@navitas.co.za>
2011-05-27 23:16:51 +0200

    Slimmer python file Signed-off-by: Tobie Nortje <tobie.nortje@navitas.co.za>

--- Logger_PC_Python_Script/pythonlogger.py
+++ Logger_PC_Python_Script/pythonlogger.py
@@ -1,21 +1,23 @@
 hallo =  """
-Arduino data logger by Tobie Nortje - tobie.nortje@navitas.co.za
-www.navitas.co.za/tobie.nortje
-https://github.com/Tooblippe/Energy_Logger/wiki
+            Arduino data logger by Tobie Nortje - tobie.nortje@navitas.co.za
+            www.navitas.co.za/tobie.nortje
+            https://github.com/Tooblippe/Energy_Logger/wiki
 """
 
-import sys
-import string
 import serial
 import time
-import eeml
+
 
 #pachube keys
 API_KEY = 'xxxxxx'
 API_URL = '/api/xxxxx.xml'
+pachube_update = 6   #roughtly update pachube every 60s
+
+#serial port setup
 port = 4
 baud = 9600
-pachube_update = 6   #roughtly update pachube every 60s
+
+#constants
 cperkwh = 72
 
 #date formatting
@@ -25,86 +27,35 @@ fmt2 = '%H:%M:%S'
 #log to filename
 filename = "C:\\temp\\latest.csv"
 
-
-
 #setup serial comms
 ser = serial.Serial(port, baud)
 
 print hallo
-print "Logging data to csv file in format DATE, TIME, POWER: " + filename
-print "Listening to port: " + str(port) + " at " +str(baud) + "baud"
-print "The following was sent by the arduino at startup"
-print "-----------------------------------------------------------------"
 print ser.readline()
 print ser.readline()              #arduino resets when connected. get rid of initial inputs sent
 
 ## now sync the time
 ## get localtime and make it unix time
-
 t = time.mktime((time.localtime()))
 ## add a T to it so the system undersands the command
 T = "T" + str(t)
-#write the command
+#write the command. THis is a T with 10 digits. The 10 digits is UNIX time, in second since the epoch..some
 ser.write(T);
 
+#serial comms with arduino start here
 print "INCOMING DATA STREAM FROM ARDUINO"
-print "----------------------------------------------------"
-print "    DATE       TIME       POWER              AVERAGE"
-print "----------------------------------------------------"
 while 1:
   val = ser.readline()
   if val > 0 :
     try:
-      f = open( filename, "a" )                       #log it to file
-      f.write( val)
+      f = open( filename, "a" )                       #Open file
+      f.write( val)  
       f.close()
     except:
-      print "that file could not be opened - " + filename
+      print "that file could not be opened - " + filename #smething went wrong
     
-    print val
+    print val #send it to the screen aswell
+
+
 # end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   # if minute == pachube_update:                         #update pachube
-   #   try:
-   #     pac = eeml.Pachube(API_URL, API_KEY)
-   #     pac.update([eeml.Data(0, average) ])
-   #     pac.put()
-   #     print "updated pachube"
-   #     print "----------------------------------------------------"
-    #    print "    DATE       TIME       POWER              AVERAGE"
-    #    print "----------------------------------------------------"
-    #    minute = 0
-    #    avarage = val
-    #  except:
-    #    print "pachube not available - will try next time"
-    #    print "----------------------------------------------------"
-    #    print "    DATE       TIME       POWER              AVERAGE"
-    #    print "----------------------------------------------------"
-    #    minute = 0
-     #   avarage = val
-
-        
-    #add the half hour incrementer here and send to different file!
